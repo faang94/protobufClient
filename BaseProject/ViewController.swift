3c591fc86e3ae32be978886d8c39408ff6a092cf
Fabio Angelilli <f.angelilli@reply.it>
2021-06-07 11:20:34 +0200

    setup without storyboard

--- BaseProject/ViewController.swift
+++ BaseProject/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  BaseProject
 //
 //  Created by Fabio Angelilli on 07/06/21.
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomeViewController: UIViewController {
+        
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        setUpLayout()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    private func setUpLayout() {
+        view.backgroundColor = .clear
+        view.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+        ])
+    }
 }
-
