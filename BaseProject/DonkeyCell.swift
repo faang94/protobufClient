d5c7b2b48ed47c550dad90178e590bbd5145e306
Fabio Angelilli <f.angelilli@reply.it>
2022-01-16 16:49:37 +0100

    first protobuf implementation

---
+++ BaseProject/DonkeyCell.swift
@@ -0,0 +1,45 @@
+//
+//  DonkeyCell.swift
+//  BaseProject
+//
+//  Created by Fabio Angelilli on 16/01/22.
+//
+
+import UIKit
+
+final class DonkeyCell: UITableViewCell {
+    
+    private var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.numberOfLines = 0
+        titleLabel.lineBreakMode = .byTruncatingTail
+        titleLabel.font = .systemFont(ofSize: 16, weight: .regular)
+        return titleLabel
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpLayout()
+        selectionStyle = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpLayout() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            contentView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 15),
+            contentView.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+        ])
+    }
+    
+    func config(with text: String) {
+        titleLabel.text = text
+    }
+}
